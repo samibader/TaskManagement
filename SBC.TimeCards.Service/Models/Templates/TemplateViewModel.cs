35208f2957248c3fa3f34d666f8ed3c924384af0
hakh <ha.kh.ni@gmail.com>
2019-03-26 09:54:10 +0200

    update for template view model to hold all data

--- SBC.TimeCards.Service/Models/Templates/TemplateViewModel.cs
+++ SBC.TimeCards.Service/Models/Templates/TemplateViewModel.cs
@@ -12,11 +12,32 @@ namespace SBC.TimeCards.Service.Models.Templates
         public int Id { get; set; }
         public int TicketId { get; set; }
         public int TemplateTypeId { get; set; }
-        public string Ip { get; set; }
-        public List<string> Disks { get; set; }
-        public List<ServerNetworkTemplate> Networks { get; set; }
+        #region Server
         public string ServerName { get; set; }
+        public string Ram { get; set; }
+        public string Cpu { get; set; }
 
-
+        public List<ServerDiskTemplateViewModel> Disks { get; set; }
+        public List<ServerNetworkTemplateViewModel> Networks { get; set; }
+        #endregion
+        #region User
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+        public string ExpirationDate { get; set; }
+        public string OU { get; set; }
+        #endregion
+        #region Network
+        public string NetworkIp { get; set; }
+        public string Subnet { get; set; }
+        public string DefaultGateway { get; set; }
+        public string Zone { get; set; }
+        #endregion
+        #region Device
+        public string Type { get; set; }
+        public string Location { get; set; }
+        public string Name { get; set; }
+        public string DeviceIp { get; set; }
+        #endregion
     }
 }
